8222a26c0030a19d916cb992a3bd9efdf17618e5
Abhinav Bora <bora.abhinav11@gmail.com>
2020-09-18 16:09:40 -0400

    Add adjustments to model and classify

--- tensorflow-practice/computer-vision.py
+++ tensorflow-practice/computer-vision.py
@@ -6,14 +6,18 @@ mnist = tf.keras.datasets.fashion_mnist
 training_images  = training_images / 255.0
 test_images = test_images / 255.0
 
+#first layer should be same shape as data, last layer should be same shape as output
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
-                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+                                    tf.keras.layers.Dense(512, activation=tf.nn.relu), 
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
 model.compile(optimizer = tf.keras.optimizers.Adam(),
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=10)
 model.evaluate(test_images, test_labels)
 
+classifications = model.predict(test_images)
+print(classifications[0])
+print(test_labels[0])
