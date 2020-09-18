1d530e3881b359c7ba910728d8665cd728c864f6
Abhinav Bora <bora.abhinav11@gmail.com>
2020-09-18 15:56:18 -0400

    Add Fashion clothing CV model

--- tensorflow-practice/test.py
+++ tensorflow-practice/test.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 
-model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1]),keras.layers.Dense(units=1, input_shape=[1])])
 
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+xs = np.array([-6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
+ys = np.array([-12.0, -10.0, -8.0, -6.0, -4.0, -2.0, 0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0], dtype=float)
 
-model.fit(xs, ys, epochs=500)
+model.fit(xs, ys, epochs=1000)
 
-print(model.predict([10.0]))
+print(model.predict([20.0]))
